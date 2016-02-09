03d06dd8ffc4a32622d093c6e07f3a5e158710b3
Aleksey Duritsyn <aleksey.duritsyn@gmail.com>
2016-02-09 10:35:46 +0100

    Build overall solution architecture. Implement interface & unit test cases for it.

---
+++ UnitTests/Test.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using WordCounterCore;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class Test
+    {
+        private IWordCounter wc;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            wc = new WordCounter();
+        }
+
+
+        static object[] staticDataPositive =
+        {
+            new object[] { "This is a statement, and so is this.", new WordCounterDic() {
+                { "this", 2 },
+                { "is", 2 },
+                { "a", 1 },
+                { "statement", 1 },
+                { "and", 1 },
+                { "so", 1 }
+                } },
+            new object[] { "Ok! No 1   R2D2 \nBuy-buy", new WordCounterDic() {
+                { "Ok", 1},
+                { "No", 1},
+                { "1", 1},
+                { "R2D2", 1},
+                { "Buy-buy", 1},
+                } },            
+        };
+
+        static object[] staticDataNegative =
+        {
+            new object[] { "", null},
+            new object[] { " ... ", null},
+            new object[] { " . , :! !? ? !", null}            
+        };
+
+        [Test, TestCaseSource("staticDataPositive")]
+        public void Calculate(string sentence, WordCounterDic expResult)
+        {            
+            WordCounterDic actResult = wc.Calculate(sentence);            
+            CollectionAssert.AreEquivalent(expResult, actResult, "Expexted and Actual Result dictionary are not match for sentence:\n" + sentence);            
+        }
+
+        [Test, TestCaseSource("staticDataNegative")]
+        public void TryCalculateIsFalse(string sentence, WordCounterDic expResult)
+        {
+            WordCounterDic actResult = null;
+            Assert.IsFalse(wc.TryCalculate(sentence, out actResult), "TryCalculate should fail with sentence:\n" + sentence);
+        }
+
+        [Test, TestCaseSource("staticDataPositive")]
+        public void TryCalculateIsTrue(string sentence, WordCounterDic expResult)
+        {
+            WordCounterDic actResult = null;
+            Assert.IsTrue(wc.TryCalculate(sentence, out actResult), "TryCalculate should pass with sentence:\n" + sentence);
+            CollectionAssert.AreEquivalent(expResult, actResult, "Expexted and Actual Result dictionary are not match for sentence:\n" + sentence);
+
+        }
+
+        [Test, TestCaseSource("staticDataPositive")]
+        public void TryCalculate(string sentence, WordCounterDic expResult)
+        {
+            WordCounterDic actResult = null;            
+            CollectionAssert.AreEquivalent(expResult, actResult, "Expexted and Actual Result dictionary are not match for sentence:\n" + sentence);
+
+        }
+    }
+}
---
+++ WordCountApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using WordCounterCore;
+
+namespace WordCountApp
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            string sentence = "";
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please enter a sentence to parse:");
+                sentence = Console.ReadLine();
+            }
+            else if (args.Length == 1)
+            {
+                sentence = args[0];
+            }
+            else
+            {
+                Console.WriteLine("Only one argument is expected.\nExample: WordCountApp.exe \"This is a statement, and so is this.\" ");
+                return -1;   
+            }
+
+            IWordCounter wc = new WordCounter();
+            WordCounterDic actResult = null;
+            if (wc.TryCalculate(sentence, out actResult))
+            {
+                foreach (KeyValuePair<string, uint> item in actResult)
+                {
+                    Console.WriteLine("{0} - {1}", item.Key, item.Value);
+                }
+
+                return 0;
+            }
+            else
+            {
+                Console.WriteLine("'{0}' is not a valid sentece.", sentence);
+                return -1;
+            }            
+        }
+    }
+}
