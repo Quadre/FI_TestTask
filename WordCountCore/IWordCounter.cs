ab010217385b5cbbbc9be29f497c9bc5fd8c07e1
Aleksey Duritsyn <aleksey.duritsyn@gmail.com>
2016-02-09 10:43:39 +0100

    Include IWordCounter project to source control sync

---
+++ WordCountCore/IWordCounter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WordCounterCore
+{
+    public class WordCounterDic : Dictionary<string, uint> { }
+
+
+    public interface IWordCounter
+    {
+        WordCounterDic Calculate(string input);
+        bool TryCalculate(string input, out WordCounterDic result);
+    }
+}
---
+++ WordCountCore/WordCounter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WordCounterCore
+{
+    public class WordCounter : IWordCounter
+    {
+        public WordCounterDic Calculate(string input)
+        {
+            throw new NotImplementedException();
+        }        
+
+        public bool TryCalculate(string input, out WordCounterDic result)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
